6c457685d03decf5664463ee17697ae6bbdebdaa
Nathan Youngman <git@nathany.com>
2023-09-28 20:13:33 -0600

    update build
    fixes #5, #9
    
    I brought over the comments from a newly generated build.zig

--- build.zig
+++ build.zig
@@ -1,7 +1,15 @@
 const std = @import("std");
 
 pub fn build(b: *std.build.Builder) !void {
+    // Standard target options allows the person running `zig build` to choose
+    // what target to build for. Here we do not override the defaults, which
+    // means any target is allowed, and the default is native. Other options
+    // for restricting supported target set are available.
     const target = b.standardTargetOptions(.{});
+
+    // Standard optimization options allow the person running `zig build` to select
+    // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall. Here we do not
+    // set a preferred release mode, allowing the user to decide how to optimize.
     const optimize = b.standardOptimizeOption(.{});
 
     // Add your executable and configure with
@@ -16,13 +24,9 @@ pub fn build(b: *std.build.Builder) !void {
     // Add the mach_glfw dependency, note the name here
     // should match the name in your build.zig.zon
     const glfw_dep = b.dependency("mach_glfw", .{
-        .target = target,
-        .optimize = optimize,
+        .target = exe.target,
+        .optimize = exe.optimize,
     });
-
-    // Now we link the resulting library,
-    // passing in the artifact from our dependency
-    exe.linkLibrary(glfw_dep.artifact("mach-glfw"));
 
     // Add the module to our package scope
     // Note the name here is the module that
@@ -31,7 +35,7 @@ pub fn build(b: *std.build.Builder) !void {
 
     // Use the mach-glfw .link helper here
     // to link the glfw library for us
-    try @import("mach_glfw").link(b, exe);
+    @import("mach_glfw").link(glfw_dep.builder, exe);
 
     // Same as above for our gl module,
     // because we copied the gl code into the project
@@ -40,24 +44,36 @@ pub fn build(b: *std.build.Builder) !void {
         .source_file = .{ .path = "libs/gl41.zig" },
     }));
 
-    // Once all is done, we install our artifact which
-    // in this case is our executable
+    // This declares intent for the executable to be installed into the
+    // standard location when the user invokes the "install" step (the default
+    // step when running `zig build`).
     b.installArtifact(exe);
 
-    // This is basic boilerplate from zig's stock build.zig,
-    // We add a run step so we can run `zig build run` to
-    // execute our program after building
+    // This *creates* a Run step in the build graph, to be executed when another
+    // step is evaluated that depends on it. The next line below will establish
+    // such a dependency.
     const run_cmd = b.addRunArtifact(exe);
+
+    // By making the run step depend on the install step, it will be run from the
+    // installation directory rather than directly from within the cache directory.
+    // This is not necessary, however, if the application depends on other installed
+    // files, this ensures they will be present and in the expected location.
     run_cmd.step.dependOn(b.getInstallStep());
+
+    // This allows the user to pass arguments to the application in the build
+    // command itself, like this: `zig build run -- arg1 arg2 etc`
     if (b.args) |args| {
         run_cmd.addArgs(args);
     }
 
+    // This creates a build step. It will be visible in the `zig build --help` menu,
+    // and can be selected like this: `zig build run`
+    // This will evaluate the `run` step rather than the default, which is "install".
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_cmd.step);
 
-    // Similar to the above but this adds tests
-    // and a test step 'zig build test'
+    // Creates a step for unit testing. This only builds the test executable
+    // but does not run it.
     const unit_tests = b.addTest(.{
         .root_source_file = .{ .path = "src/main.zig" },
         .target = target,
@@ -66,6 +82,9 @@ pub fn build(b: *std.build.Builder) !void {
 
     const run_unit_tests = b.addRunArtifact(unit_tests);
 
+    // Similar to creating the run step earlier, this exposes a `test` step to
+    // the `zig build --help` menu, providing a way for the user to request
+    // running the unit tests.
     const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&run_unit_tests.step);
 }
